4afef840e1e6349acbe9873eb13f3f1db68d18ae
Samuel Razzell <Sam@Samuels-MacBook-Air.local>
2013-12-08 22:21:43 -0800

    Tutorial version 1 done

--- src/edu/berkeley/SouthsideSeniors/HeightTracker/Tutorial.java
+++ src/edu/berkeley/SouthsideSeniors/HeightTracker/Tutorial.java
@@ -1,9 +1,13 @@
 package edu.berkeley.SouthsideSeniors.HeightTracker;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class Tutorial extends Activity {
 
@@ -12,6 +16,9 @@ public class Tutorial extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tutorial);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		String current_user = preferences.getString("current_user", "");
+		setTitle(current_user);
 	}
 
 	@Override
@@ -19,6 +26,18 @@ public class Tutorial extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.tutorial, menu);
 		return true;
+	}
+	
+	public boolean users(MenuItem item){
+		Intent i = new Intent(this, ViewUsers.class);
+		startActivity(i);
+		return true;
+	}
+	
+	@Override
+	public void onBackPressed() {
+		Intent i = new Intent(this, Measure.class);
+		startActivity(i);
 	}
 
 }
